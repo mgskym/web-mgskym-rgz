6fe44dcde14e84205de05428cc920b899b846d6b
Michael Maglitscky <mihaillel@yandex.ru>
2023-12-01 00:36:57 +0700

    Файлы+

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, Blueprint, render_template, request, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('base.html')
