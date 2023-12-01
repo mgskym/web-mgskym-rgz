bd7be486f884349c138a764710f09f6661267d4c
Michael Maglitscky <mihaillel@yandex.ru>
2023-12-02 00:36:39 +0700

    база данных

--- app.py
+++ app.py
@@ -7,7 +7,19 @@ from db.models import users
 
 app = Flask(__name__)
 
+
+app.secret_key = '123'
+user_db = "mgskym_rgz_web"
+host_ip = "localhost"
+host_port = "5432"
+database_name = "pharmacy_database"
+password = "333"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
 @app.route('/')
 def main():
-    username = (users.query.first()).username
+    username = (users.query.first())
     return render_template('main_list.html', username=username)
